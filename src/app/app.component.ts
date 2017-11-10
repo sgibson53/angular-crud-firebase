120726230c16cdecf6ebe7ba8e3491fa963f9515
Seth Gibson <sethg@Seths-MacBook-Pro.local>
2017-11-10 14:26:29 -0600

    implementing firbase..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore'; 
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items: Observable<any[]>;
+  msg: string = '';
+
+  constructor(public af: AngularFirestore) {
+    this.items = af.collection('/messages').valueChanges();
+    this.items.subscribe(theitems => {
+      console.log(theitems);
+    })
+  }
+
+  send(chatMsg: string) {
+    // this.items.push({ message: chatMsg })
+    this.msg = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6r-j3si2QvNjV7nZ1uNOBC3MGfkdVrC8",
+  authDomain: "chatapp-2d48e.firebaseapp.com",
+  databaseURL: "https://chatapp-2d48e.firebaseio.com",
+  projectId: "chatapp-2d48e",
+  storageBucket: "",
+  messagingSenderId: "266051866732"
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
